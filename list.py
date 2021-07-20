02b8295675a0787484306c047c0c56dd108e374a
Ramesh Ram <ramesh.r3@ibm.com>
2021-07-20 15:46:29 +0530

    data structures

--- list.py
+++ list.py
@@ -1,2 +1,9 @@
-name=["dhoni","raina","virat"]
-print(name[1:2])
+name=["dhoni","raina","virat",7,10,3.18]
+del name[3]
+print(name)
+name.remove("virat")
+print(name)
+name.pop(1)
+print(name)
+name.clear()
+print(name)
