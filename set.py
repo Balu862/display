e7c4d878df6e6cd835d7281fec690f32a7f29a7c
Ramesh Ram <ramesh.r3@ibm.com>
2021-07-21 14:25:36 +0530

    adding sets

---
+++ set.py
@@ -0,0 +1,7 @@
+fruits={"orange","apple","mango","apple"}
+juice={"orange","pineapple"}
+print(fruits.intersection(juice))
+print(fruits.union(juice))
+print(juice.issuperset(fruits))
+print(juice.issubset(fruits))
+print(fruits.difference(juice))
