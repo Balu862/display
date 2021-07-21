ab169a1aa45fc7b7914ca23f20123b8967fc6c93
Ramesh Ram <ramesh.r3@ibm.com>
2021-07-21 14:13:37 +0530

    students data

---
+++ students.py
@@ -0,0 +1,5 @@
+data=[{"name":"Balu","rollno":"123","adminum":"456","college":"ifet","marks":{"eng":50,"maths":60,"phy":70}},
+{"name":"MSD","rollno":"7","adminum":"123","college":"IFET","marks":{"eng":20,"maths":40,"phy":60}}]
+print(data)
+data[0]["name"]="bala"
+print(data)
