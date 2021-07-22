e57ce0b7418e0bcf53977698e014c27d43dfdaaa
Ramesh Ram <ramesh.r3@ibm.com>
2021-07-22 10:56:10 +0530

    adding palindrome

---
+++ strings.py
@@ -0,0 +1,14 @@
+'''data=input("Enter a string:")
+x=slice(7)
+print(data)
+print(data[x])
+print(data.capitalize())
+print(data.title())
+print(data.upper())
+print(data.lower())
+print(data.strip())
+print(data.find("world"))
+print(data.count("hello"))
+print(data.replace("hello","hi"))'''
+x="abc"
+print(x[:2])
